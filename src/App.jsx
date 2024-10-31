9c0e39e9f2f7778263d4de4117edc9b016f0d49f
unknown <arthurpedroti@gmail.com>
2024-10-30 21:21:22 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Sidebar as Teste} from './components/Navbar'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <p className="test">Hello world</p>
+      <p style={{
+        color: 'blue'
+      }}>Hello world</p>
+      <Teste />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+   <p>Navbar</p>
+  )
+}
+
+export function Sidebar() {
+  return (
+   <p>Sidebar</p>
+  )
+}
+
+
+export default Navbar
